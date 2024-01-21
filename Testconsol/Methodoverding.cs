bfd81508959d1d8ebdae81702eaa7bf66e73142b
admin <admin@DESKTOP-RGF6FSL>
2024-01-22 00:27:21 +0530

    Add project files.

---
+++ Testconsol/Methodoverding.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Testconsol
+{
+    public class Methodoverding
+    {
+        public string method1()
+        {
+            string s = "hello SIR";
+        }
+        
+    }
+    public class manager: Methodoverding
+    {
+        public override string method1() 
+        {
+
+        }
+    }
+}
---
+++ Testconsol/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+public class Test
+{
+    public static void Main(string[] args)
+    {
+        int a = 20;
+        int b = 10;
+        int sum = 0;
+        Addition(a, b, sum);
+        Console.WriteLine(sum);
+    }
+    public static void Addition(int a, int b, int sum)
+    {
+        sum=a+b;
+        Console.WriteLine(sum);
+    }
+}
